833b74683dea302bbc751c1f578174f46e20463c
Kiran Silwal <kiransilwal2002@gmail.com>
2025-09-05 19:17:57 -0500

    update env values

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -37,6 +37,7 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
                     searchTerm: query,
                     movie_id: movie.id,
                     count: 1,
+                    title: movie.title,
                     poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
                 }
             );
